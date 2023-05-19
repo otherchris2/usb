754e05b79e5b338510905a14b8f9852e2cac5422
jtaborda03 <112491126+jtaborda03@users.noreply.github.com>
2023-05-19 11:21:53 -0500

    g

--- JuanTab.php
+++ JuanTab.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
     echo "pollo";
 
     $client = new MongoDB\Client(
-        ' mongodb+srv://jtaborda0308:<password>@cluster0.ugwukri.mongodb.net/?retryWrites=true&w=majority');
+        ' mongodb+srv://jtaborda0308@cluster0.ugwukri.mongodb.net/?retryWrites=true&w=majority');
 
     $con=$client ->test ->user;
     $cur=$con->find();
