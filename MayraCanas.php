281ab39c26c407c040190c683f5d8bd6030fee2e
Chronoka11 <133896696+Chronoka11@users.noreply.github.com>
2023-05-19 10:52:19 -0500

    q

--- MayraCanas.php
+++ MayraCanas.php
@@ -4,4 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 $client = new MongoDB\Client(
     'mongodb+srv://macanasq:<password>@cluster0.zlf96tu.mongodb.net/?retryWrites=true&w=majority');
 
+    $con=$client->Test->users;
+
+    $cur=$con->find(); //Select from
+    $datos=iterator_to_array($cur);
+    echo $datos;
 ?>    
