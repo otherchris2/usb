fe1006449385f68d69b82306752023f6b74ae422
jdbustos <133896816+jdbustos@users.noreply.github.com>
2023-05-19 09:06:34 -0600

    Add files via upload

---
+++ dato_jd.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . 'dato_jd.php';
+//Variables de entrada
+echo "awaw";
+//--------------------------------------
+$client = new MongoDB\Client(
+    'mongodb+srv://jdbustos:<password>@cluster0.vwpxcac.mongodb.net/?retryWrites=true&w=majority');
+
+    
+
+?>
