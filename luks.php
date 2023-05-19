1a9039479bb67ae86b58aa6f7ba63a81691c5d7a
pannotostado <112526906+pannotostado@users.noreply.github.com>
2023-05-19 11:25:24 -0500

    a

--- luks.php
+++ luks.php
@@ -3,6 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 //Variables de entrada
 
+echo "jijijiju";
+
 //--------------------------------------
 $client = new MongoDB\Client(
     'mongodb+srv://santichi:lukassantiago809@cluster0.dv2ss6e.mongodb.net/?retryWrites=true&w=majority');
