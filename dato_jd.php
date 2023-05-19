946805cfca59836f12187942d9cf1af419b0fc35
jdbustos <133896816+jdbustos@users.noreply.github.com>
2023-05-19 10:22:04 -0600

    a

--- dato_jd.php
+++ dato_jd.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 //Variables de entrada
-echo "awaw";
+//echo "awaw";
 //--------------------------------------
 $client = new MongoDB\Client(
     '
