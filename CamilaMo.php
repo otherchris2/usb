cba5fe5721507e31974c47a510bd8bca9438a022
CamMo02 <133913753+CamMo02@users.noreply.github.com>
2023-05-19 10:54:15 -0500

    c

--- CamilaMo.php
+++ CamilaMo.php
@@ -4,4 +4,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $client = new MongoDB\Client('mongodb+srv://ClaseBD:ClaseDB02@cluster0.qn4pt8b.mongodb.net/?retryWrites=true&w=majority');
 
+$con=$client->Test->post;
+
+$cur=$con->find();
+$datos= iterator_to_array($cur);
+echo $datos;
+
 ?>
