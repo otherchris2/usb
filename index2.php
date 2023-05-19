f4124c33478782c29d0b4584e4d8ce6287fa2563
otherchris2 <105050309+otherchris2@users.noreply.github.com>
2023-05-19 10:09:09 -0500

    m

--- index2.php
+++ index2.php
@@ -1,3 +1,6 @@
 <?php
-echo ("Cualquer cosa");
+require_once __DIR__. '/vendor/autoload.php';
+$client = new MongoDB\Client('mongodb+srv://yo:abc13579@cluster0.bosd3.mongodb.net/lluvia?retryWrites=true&w=majority');
+
+
 ?>
