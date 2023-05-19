25bf6c7eec3cb6755c5d90958a9a51436bae10bd
CamMo02 <133913753+CamMo02@users.noreply.github.com>
2023-05-19 10:44:04 -0500

    c

---
+++ CamilaMo.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$client = new MongoDB\Client('mongodb+srv://ClaseBD:ClaseDB02@cluster0.qn4pt8b.mongodb.net/?retryWrites=true&w=majority');
+
+?>
