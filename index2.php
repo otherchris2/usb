f940cf1bb13a8e2c3939ef0ff06811483604ed18
otherchris2 <105050309+otherchris2@users.noreply.github.com>
2023-05-19 10:49:39 -0500

    r

--- index2.php
+++ index2.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__. '/vendor/autoload.php';
-$client = new MongoDB\Client('mongodb+srv://yo:abc13579@cluster0.bosd3.mongodb.net/lluvia?retryWrites=true&w=majority');
+$client = new MongoDB\Client('mongodb+srv://yo:abc13579@cluster0.bosd3.mongodb.net/?retryWrites=true&w=majority');
 
+$con=$client->TallerAuto->Vehiculo;
+
+//"Select *from vehiculo"
+$cur=$con->find();
+//************** */
+$datos=iterator_to_array($cur);
+echo $datos;
 
 ?>
