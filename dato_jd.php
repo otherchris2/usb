bd5bf3d93ddd8a1f68e97f4c4566fd89ebf78814
jdbustos <133896816+jdbustos@users.noreply.github.com>
2023-05-19 09:48:04 -0600

    a

--- dato_jd.php
+++ dato_jd.php
@@ -7,6 +7,9 @@ echo "awaw";
 $client = new MongoDB\Client(
     'mongodb+srv://jd:12345@cluster0.vwpxcac.mongodb.net/?retryWrites=true&w=majority');
 
-    
+   $con=$client ->test ->user;
+   $cur=$con->find();
+   $datos=iterator_to_array($cur);
+   echo $datos;
 
 ?>
