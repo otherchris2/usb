b99319c3d56ac2045bdd59a561e02a6bc213af01
vale <102614456+Valegonz19@users.noreply.github.com>
2023-05-19 10:44:17 -0500

    a

--- Valeria_Gonzalez.php
+++ Valeria_Gonzalez.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-//Variables de entrada//--------------------------------------
+
 $client = new MongoDB\Client(
     'mongodb+srv://Basedatos:Base123@cluster0.bosd3.mongodb.net/lluvia?retryWrites=true&w=majority');
     ?>
