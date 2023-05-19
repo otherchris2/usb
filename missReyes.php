8a3a240a2c04c829e026c4d360ef1c33aeecd712
TheRealKingsMan <103980090+TheRealKingsMan@users.noreply.github.com>
2023-05-19 10:44:23 -0500

    ss

--- missReyes.php
+++ missReyes.php
@@ -1,12 +1,7 @@
 <!-- Hola me llamo santiago -->
 <?php
-
-
     echo "Se ha agregado tu curso"."<br>";
     require_once __DIR__ . '/vendor/autoload.php';
-    //Variables de entrada
-    
-    //--------------------------------------
     $client = new MongoDB\Client(
     'mongodb+srv://cristian:bases123@cluster0.bosd3.mongodb.net/lluvia?retryWrites=true&w=majority');
 
