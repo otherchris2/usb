d2c67edb8c0a8e93d54470fbaaddd1ae67bc1791
pannotostado <112526906+pannotostado@users.noreply.github.com>
2023-05-19 11:11:51 -0500

    zi

--- dato_jd.php
+++ dato_jd.php
@@ -7,7 +7,7 @@ echo "awaw";
 $client = new MongoDB\Client(
     'mongodb+srv://jd:12345@cluster0.vwpxcac.mongodb.net/?retryWrites=true&w=majority');
 
-   $con=$client ->test ->user;
+   $con=$client ->Test ->user;
    $cur=$con->find();
    $datos=iterator_to_array($cur);
    echo $datos;
