0de8241c285b67bb3d7fbabd8b45f49a7199b5de
pannotostado <112526906+pannotostado@users.noreply.github.com>
2023-05-19 11:16:03 -0500

    zzi

--- haider.php
+++ haider.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/vendor/autoload.php';
     $client = new MongoDB\Client(
         'mongodb+srv://hadyr:1234567890@cluster0.ugfimkb.mongodb.net/?retryWrites=true&w=majority');
 
-    $con=$client ->Test ->user;
+    $con=$client ->datanombre ->zi;
     $cur=$con->find();
     $datos=iterator_to_array($cur);
     echo $datos;
