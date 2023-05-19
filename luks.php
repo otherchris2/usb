875213b095dec3aba4bf73efcd235017231a0b5b
luks1023 <134080693+luks1023@users.noreply.github.com>
2023-05-19 11:22:42 -0500

    a

---
+++ luks.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+//Variables de entrada
+
+//--------------------------------------
+$client = new MongoDB\Client(
+    'mongodb+srv://santichi:lukassantiago809@cluster0.dv2ss6e.mongodb.net/?retryWrites=true&w=majority');
+   
+    $con=$client ->test ->users;
+    $cur=$con->find();
+    $datos=iterator_to_array($cur);
+    echo $datos;
+?>
+
+
+
