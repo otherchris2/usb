d7d531b2c1b4e3b3af8fbc889f0ab4b6eb921490
pannotostado <112526906+pannotostado@users.noreply.github.com>
2023-05-19 11:18:25 -0500

    zi

--- haider.php
+++ haider.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
     echo "jijijiju";
 
-    $client = new MongoDB\Client(
+$client = new MongoDB\Client(
         'mongodb+srv://hadyr:1234567890@cluster0.ugfimkb.mongodb.net/?retryWrites=true&w=majority');
 
     $con=$client ->datanombre ->zi;
