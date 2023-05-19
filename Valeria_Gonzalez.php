281ab49602b28cfd2fcd498421e067eb3ec7e4de
vale <102614456+Valegonz19@users.noreply.github.com>
2023-05-19 10:05:20 -0500

    v2

--- Valeria_Gonzalez.php
+++ Valeria_Gonzalez.php
@@ -1,1 +1,9 @@
-holaaaaaaa
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+//Variables de entrada
+$a="1995";
+$b="Pajas Blancas";
+//--------------------------------------
+$client = new MongoDB\Client(
+    'mongodb+srv://yo:abc13579@cluster0.bosd3.mongodb.net/lluvia?retryWrites=true&w=majority');
