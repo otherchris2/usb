aa70082368dc1295f7a0ff2d665889e01b3e29e5
esteban247 <83054253+esteban247@users.noreply.github.com>
2023-05-24 00:10:53 -0500

    esteban

--- esteban.php
+++ esteban.php
@@ -1,16 +1,12 @@
 <?php
-    echo "Se ha agregado tu curso"."<br>";
+    echo "HOLA"."<br>";
     require_once __DIR__ . '/vendor/autoload.php';
     $client = new MongoDB\Client(
     'mongodb+srv://esteban:esteban1234@cluster0.68xawn4.mongodb.net/?retryWrites=true&w=majority');
 
     $con=$client->Test->user;
 
-    $cur=$con->find();
-    foreach($cur as $document){
-        echo $document["name"] . "<br>";
-        
-    }
-
-
-?>
+    $cur=$con->find(); //Select from
+    $datos=iterator_to_array($cur);
+    echo $datos;
+?>    
